b30df2a1599888c4a45131f5bb480937c688a72c
michlecc8 <habs.michael@gmail.com>
2015-07-22 18:40:22 +0000

    sql table migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require './config/environment'
 require './app/models/tweet'
 require 'pry'
@@ -11,9 +10,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    @tweets = Tweet.all
     erb :index
   end
-
-
+  
+  get '/tweet' do
+    erb :tweet
+  end
+  
+  post '/tweet' do
+    tweet = Tweet.new({:user => params[:user], :status => params[:status]})
+    tweet.save
+    redirect '/'
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,17 @@
-class Tweet
-
+class Tweet < ActiveRecord::Base
+  
+#   attr_accessor :user, :status
+  
+#   ALL = []
+  
+#   def initialize(user,status)
+#     @user = user
+#     @status = status
+#     ALL << self #takes the tweet, and puts it into the all tweets array
+#   end
+  
+#   def self.all
+#     ALL
+#   end
+  
 end
